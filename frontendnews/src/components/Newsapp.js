00dba0612f6e8352e68ab74c382820316e05f9ac
Rahul Kumar <mynamerahulkumar13@gmail.com>
2025-04-03 19:46:35 +0100

    fastapi added

--- frontendnews/src/components/Newsapp.js
+++ frontendnews/src/components/Newsapp.js
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 const Newsapp = () => {
-    const [search, setSearch] = useState("crypto latest");
+    const [search, setSearch] = useState("crypto");
     const [newsData, setNewsData] = useState(null)
     const API_KEY = "10bf3b99ea114e5bbb35a9b892f62080";
 
-    const getData = async() =>{
-        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+    const getData = async( searchTerm=search) =>{
+        // const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+        const currentDate=new Date();
+        console.log('date-',currentDate);
+        currentDate.setDate(currentDate.getDate()-1);
+        const yesterday= currentDate.toISOString().split('T')[0];
+        const sortBy="popularity";
+        const response=await fetch(`https://newsapi.org/v2/everything?q=${search}&from=${currentDate}&sortBy=${sortBy}&apiKey=${API_KEY}`) 
         const jsonData = await response.json();
         console.log(jsonData.articles);
         let dt = jsonData.articles.slice(0,10)
@@ -22,11 +28,16 @@ const Newsapp = () => {
     const handleInput = (e) =>{
         console.log("handle_input",e.target.value);
         setSearch(e.target.value)
-        getData();
+       
         
     }
-    const userInput = (event) =>{
-        setSearch(event.target.value)
+    const userInput = (e) =>{
+        console.log('tag-',e.target.value)
+       
+        const newSearch=e.target.value;
+        setSearch(newSearch);
+        getData(newSearch);
+        
     }
 
   return (
@@ -49,11 +60,13 @@ const Newsapp = () => {
             <p className='head'>Stay Update with Crypto and Stock News</p>
         </div>
         <div className='categoryBtn'>
-            <button onClick={userInput} value="sports">Crypto latest</button>
+            <button onClick={userInput} value="Crypto">Crypto latest</button>
             <button onClick={userInput} value="USA Trump">USA Trump</button>
             <button onClick={userInput} value="ethereum">ethereum</button>
             <button onClick={userInput} value="NSE India">NSE India</button>
-            <button onClick={userInput} value="India stock Market latest">India stock Market latest</button>
+            <button onClick={userInput} value="India stock Market">India stock Market </button>
+            <button onClick={userInput} value="IPL T20">IPL T20</button>
+            
         </div>
 
         <div>
